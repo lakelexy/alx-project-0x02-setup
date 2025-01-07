4b75eaba71f83cbd365d7b411a79968205c8409a
lakelexy <alao8380@yahoo.com>
2025-01-07 10:29:53 +0100

    Basic Routing in Next.js

---
+++ alx-project-2/pages/about.tsx
@@ -0,0 +1,12 @@
+const AboutPage = () => {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold">About Us</h1>
+      <p className="mt-4 text-lg text-gray-700">
+        This is the about page of our site.
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ alx-project-2/pages/home.tsx
@@ -0,0 +1,12 @@
+const HomePage = () => {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold">Welcome to the Home Page</h1>
+      <p className="mt-4 text-lg text-gray-700">
+        This is the home page of our site.
+      </p>
+    </div>
+  );
+};
+
+export default HomePage;
