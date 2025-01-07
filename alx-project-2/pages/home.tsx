b0618bd681e62034985df13bb5ae6075075ef942
lakelexy <alao8380@yahoo.com>
2025-01-07 11:00:34 +0100

    Creating a Reusable Card Component

--- alx-project-2/pages/home.tsx
+++ alx-project-2/pages/home.tsx
@@ -1,3 +1,5 @@
+import Card from "../components/common/Card";
+
 const HomePage = () => {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
@@ -5,6 +7,14 @@ const HomePage = () => {
       <p className="mt-4 text-lg text-gray-700">
         This is the home page of our site.
       </p>
+
+      {/* Card 1 */}
+      <Card title="Card 1" content="This is the content for the first card." />
+
+      <div className="my-4" />
+
+      {/* Card 2 */}
+      <Card title="Card 2" content="This is the content for the second card." />
     </div>
   );
 };
