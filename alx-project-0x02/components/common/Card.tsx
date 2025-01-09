2cacbc030793607ed9fabdd36b020b281c990464
lakelexy <alao8380@yahoo.com>
2025-01-09 11:57:39 +0100

    adding new code of card.tsx

--- alx-project-0x02/components/common/Card.tsx
+++ alx-project-0x02/components/common/Card.tsx
@@ -1,14 +1,10 @@
-// components/common/Card.tsx
-import React from "react";
 import { CardProps } from "@/interfaces";
 
-const Card: React.FC<CardProps> = ({ title, content }) => {
+export default function Card({ title, content }: CardProps) {
   return (
-    <div className="bg-white p-6 rounded-lg shadow-md w-64">
-      <h2 className="text-xl font-bold mb-4">{title}</h2>
+    <div>
+      <h2>{title}</h2>
       <p>{content}</p>
     </div>
   );
-};
-
-export default Card;
+}
